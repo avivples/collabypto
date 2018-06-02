4562f85dfd1271bc3a0bf98dc0c706e46d272306
avivples <aviv.ples1@gmail.com>
2018-06-02 16:49:30 +0300

    Implementation of Client side gui, and also some functionality of collab client

---
+++ src/gui/LoginPage.java
@@ -0,0 +1,239 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+import server_client.CollabClient;
+
+public class LoginPage extends JFrame{
+
+    /** Default local IP address */
+    private static final String LOCAL_IP = "127.0.0.1";
+    /** Default server port to connect */
+    private static final int DEFAULT_PORT = 4444;
+    /** Default user name if the user doesn't supply one */
+    private static final String DEFAULT_USERNAME = "Anonymous";
+
+    /** The default width of the Window Frame */
+    private static final int WIDTH = 800;
+    /** The default height of the Window Frame */
+    private static final int HEIGHT = 400;
+    /** Prompt to the user */
+    private static final String PROMPT = "Please Enter user name, IP address"
+            + " and Port or Default for local connection";
+    /** Prompt quesion for IP adress */
+    private static final String IP = "Server IP";
+    /** Prompt quesion for PORT */
+    private static final String PORT = "Server Port";
+    /** The constant for JLabel in Provided Information Page */
+    private static final String USER_NAME = "User Name";
+    /** contants represents string for default button */
+    private static final String DEFAULT_BUTTON = "Default";
+    /** contants represents string for submit button */
+    private static final String SUBMIT_BUTTON = "Submit";
+
+    /** User defined ip */
+    private JTextField ipInput;
+    /** user names of the user connects */
+    private JTextField userNameInput;
+    /** User defined port */
+    private JTextField portInput;
+
+    /** port number of the server */
+    private int portNo;
+    /** An instance to collabClient */
+    private CollabClient collabClient;
+
+    /** notify user when they enter wrong port number */
+    protected static final String WRONG_PORT_NUMBER = "Please enter correct port number (xx.xxx.xx.xx)";
+    /** notify user when they enter wrong IP or port number */
+    private static final String WRONG_IP_OR_PORT = "Please correct your IP and Port number";
+
+
+    public LoginPage() { initGui(); }
+
+    private void initGui() {
+
+        final JFrame frame = new JFrame("Provide information");
+
+        JPanel pane = new JPanel(new BorderLayout());
+
+        JLabel prompt = new JLabel(PROMPT);
+        JLabel userName = new JLabel(USER_NAME);
+        userNameInput = new JTextField();
+        userNameInput.setToolTipText("Enter your user name");
+
+        JLabel ip = new JLabel(IP);
+        ipInput = new JTextField();
+
+        ipInput.setToolTipText("Enter the ip address");
+
+        JLabel port = new JLabel(PORT);
+        portInput = new JTextField();
+        portInput.setToolTipText("Enter the port number");
+        JButton defaultVal = new JButton(DEFAULT_BUTTON);
+        JButton submit = new JButton(SUBMIT_BUTTON);
+
+        String type = "server_client";
+
+        defaultVal.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                String userName = userNameInput.getText();// get the Text From
+                // the user
+                if (userName.length() <= 0) {
+                    userName = DEFAULT_USERNAME;
+                } else {
+                    userName = userNameInput.getText().trim();
+                }
+                frame.dispose();
+                collabClient = new CollabClient(LOCAL_IP, DEFAULT_PORT, userName);
+
+                Thread thread = new Thread() {
+                    @Override
+                    public void run() {
+                        collabClient.start();
+                    }
+                };
+
+                thread.start();
+            }
+        });
+        // connect to remote server
+        submit.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String userName = userNameInput.getText();// get the Text From
+                // the user
+                if (userName.length() <= 0) {
+                    userName = DEFAULT_USERNAME;
+                } else {
+                    userName = userNameInput.getText().trim();
+                }
+                String ip = ipInput.getText().trim();
+                String portInputString = portInput.getText().trim();
+                try {
+                    if (ip.length() > 0 && portInputString.length() > 0) {
+                        try {
+                            // take the port number input
+                            portNo = Integer.parseInt(portInputString);
+
+                        } catch (NumberFormatException exception) {
+                            frame.dispose();
+                            new ErrorDialog(WRONG_PORT_NUMBER);
+                            SwingUtilities.invokeLater(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    LoginPage.this.initGui();
+
+                                }
+                            });
+
+                        }
+                        frame.dispose();
+                        collabClient = new CollabClient(ip, portNo, userName);
+                        Thread thread = new Thread() {
+                            @Override
+                            public void run() {
+                                collabClient.start();
+                            }
+                        };
+
+                        thread.start();
+                    } else {
+                        frame.dispose();
+                        new ErrorDialog(WRONG_IP_OR_PORT);
+                        SwingUtilities.invokeLater(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                LoginPage.this.initGui();
+
+                            }
+                        });
+
+                    }
+                } catch (IllegalArgumentException exception) {
+                    frame.dispose();
+                    new ErrorDialog(WRONG_IP_OR_PORT);
+                    SwingUtilities.invokeLater(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            LoginPage.this.initGui();
+
+                        }
+                    });
+
+                }
+            }
+        });
+
+        pane.add(prompt, BorderLayout.PAGE_START);
+        JPanel ipPortPane = new JPanel();
+        GroupLayout ipPortLayout = new GroupLayout(ipPortPane);
+        ipPortPane.setLayout(ipPortLayout);
+        ipPortLayout.setAutoCreateGaps(true);
+
+        ipPortLayout.setAutoCreateContainerGaps(true);
+
+        // Create a sequential group for the horizontal axis.
+        GroupLayout.SequentialGroup hGroup = ipPortLayout
+                .createSequentialGroup();
+        hGroup.addGroup(ipPortLayout.createParallelGroup()
+                .addComponent(userName).addComponent(ip).addComponent(port));
+        hGroup.addGroup(ipPortLayout.createParallelGroup()
+                .addComponent(userNameInput).addComponent(ipInput)
+                .addComponent(portInput));
+        ipPortLayout.setHorizontalGroup(hGroup);
+
+        // Create a sequential group for the vertical axis.
+        GroupLayout.SequentialGroup vGroup = ipPortLayout
+                .createSequentialGroup();
+        vGroup.addGroup(ipPortLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                .addComponent(userName).addComponent(userNameInput));
+        vGroup.addGroup(ipPortLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                .addComponent(ip).addComponent(ipInput));
+        vGroup.addGroup(ipPortLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                .addComponent(port).addComponent(portInput));
+        ipPortLayout.setVerticalGroup(vGroup);
+        ipPortPane.setPreferredSize(new Dimension(200, 100));
+        pane.add(ipPortPane, BorderLayout.CENTER);
+
+        JPanel clickPane = new JPanel();
+        clickPane.add(defaultVal);
+        clickPane.add(submit);
+        pane.add(clickPane, BorderLayout.PAGE_END);
+        frame.setContentPane(pane);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new LoginPage();
+
+            }
+        });
+
+
+    }
+}
---
+++ src/server_client/CollabClient.java
@@ -0,0 +1,294 @@
+package server_client;
+
+import document.*;
+import model.CollabModel;
+import gui.ClientGui;
+import gui.DocumentSelectionPage;
+import gui.ErrorDialog;
+
+import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+/**
+ * This is the client that will connect to a server. A unique instance of the
+ * client will be created each time a user opens up a client. It will receive a
+ * copy of the central document from the server. Changes to the document on this
+ * particular client will be sent to the server. Changes to the document by
+ * other clients will be relayed by the server, and this client will apply an OT
+ * algorithm to correctly update its own document. All of this will be reflected
+ * on the GUI.
+ *
+ *
+ * Passing these tests ensure that our client is able to display the latest
+ * version of the document while a user is adding new text.
+ *
+ * @author youyanggu
+ *
+ */
+public class CollabClient implements CollabInterface {
+    /** Timeout on socket connection attempts */
+    private static final int TIMEOUT = 2000;
+    /** unique to each client. Used to differentiate operations */
+	private int siteID;
+	/** document the client is editing */
+	private String document = "";
+
+	/** port number of client */
+	private int port;
+	/** ip number of client */
+	private String ip;
+	/** username of client */
+	private String name;
+
+	/** label to display at the top of the document GUI */
+	protected String label = "Client";
+	/** Intializes socket and streams to null */
+	private Socket s = null;
+
+	/** outputstream to send objects to server */
+	protected ObjectOutputStream out = null;
+	/** inputstream to receive objects from server */
+	protected ObjectInputStream in = null;
+	/** client GUI used to display the document */
+	protected ClientGui gui;
+
+	/**
+	 * Constructor to start the client. Simply sets the identifiers.
+	 * The connection to the server come later in the connect() call.
+	 *
+	 * @param IP - the ip address of the host to connect to
+	 * @param port - the port number of the host to connect to
+	 * @param name - the name alias given to the client
+	 */
+	public CollabClient(String IP, int port, String name) {
+		this.ip = IP;
+		this.port = port;
+		this.name = name;
+	}
+
+	/**
+	 * start up a client.
+	 * by calling the connect() method.
+	 * @throws OperationEngineException if the operation finds an incosistency
+	 */
+	public void start() {
+        try {
+            this.connect();
+        } catch (IOException e) {
+            new ErrorDialog(e.toString());
+        }
+
+	}
+
+
+	/**
+	 * Starts the client and try to connec to the server with the paramters
+	 * given in the constructor. It success, it will continue a connection with
+	 * the server until an exception is thrown. A GUI is created that will
+	 * hopefully reflect the current state of the document. Various message
+	 * passing will be used to send insert/delete updates. The document will
+	 * hopefully be updated with edits made by both the client itself and other
+	 * clients, relayed by the server.
+	 *
+	 * @throws IOException if the socket is broken or corrupted
+	 * @throws OperationEngineException if the operation caused an exception when being processed by
+     * the operation engine
+	 */
+	public void connect() throws IOException {
+
+	    // Establishes a socket connection
+		System.out.println("Connecting to port: " + this.port + " at: " + this.ip);
+		InetSocketAddress address = new InetSocketAddress(this.ip, this.port);
+		try {
+		    s = new Socket();
+		    s.connect(address, TIMEOUT);
+		} catch (UnknownHostException e) {
+            System.err.println("Don't know about host: " + this.name);
+            return;
+        } catch (IOException e) {
+		    System.err.println("Timeout: cannot connect to IP: " + this.ip + " , port: " + this.port + ". Please try again.");
+		    return;
+		} catch (Exception e) {
+		    System.err.println("Your port number (" + this.port + ") or IP (" + this.ip + ") is incorrect. Please try again");
+		    return;
+		}
+
+		// Connection established. Communicates with server
+		try {
+			out = new ObjectOutputStream(s.getOutputStream());
+			in = new ObjectInputStream(s.getInputStream());
+
+			Object o= in.readObject();
+
+			if(!(o instanceof ArrayList<?>)) {
+                throw new RuntimeException("Expected ArrayList of documents");
+            }
+
+			// Popup for user to select document. We already checked above for object type
+            @SuppressWarnings("unchecked")
+            JFrame f = new DocumentSelectionPage((ArrayList<String>) o,this);
+
+            // Waits until user selects document to edit. Calls setDocument() on return
+            // to set the document to edit
+		    while (f.isVisible()) {
+		        try {
+		            Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+		    }
+
+		    // Sends to server the document this client wants to edit
+			out.writeObject(document);
+			out.flush();
+
+			// Reads in operations from the server
+			o = in.readObject();
+			while (o != null) {
+			    parseInput(o);
+				o = in.readObject();
+
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+		    // Close connection
+		    s.close();
+			out.close();
+			in.close();
+			System.exit(0);
+
+		}
+	}
+
+	/**
+	 * Takes an object that was received from the server and parses it according
+	 * to its type. Performs the appropriate calls and updates based on the object received
+	 * @param o - object sent from the server to parse
+	 * @throws IOException - caused if the operation object is corrupt, or if the socket connection breaks
+	 * @throws OperationEngineException - if the operation caused an exception when being processed by
+	 * the operation engine
+	 */
+	@SuppressWarnings("unchecked")
+    public void parseInput(Object o) throws IOException {
+	    if (o instanceof Operation) {
+	        // We received an operation from the server. Update the local document
+            updateDoc((Operation) o);
+        } else if (o instanceof Integer) {
+            // The server is sending the unique client identifiers
+            this.siteID = ((Integer) o).intValue();
+            if (this.name.equals("Anonymous"))
+                this.name += "" + this.siteID;
+            out.writeObject(this.name);
+            out.flush();
+            label = this.name + " is editing document: " + this.document;
+
+        } else if (o instanceof String) {
+            // The server just sent the initial string of the document
+            // Start up the GUI with this string
+            try {
+                this.gui = new ClientGui((String) o, this, label);
+            } catch (OperationEngineException e) {
+                e.printStackTrace();
+            }
+            this.gui.setModelKey(document);
+
+            JFrame frame = new JFrame("Collab Edit Demo");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            // Add content to the JFrame window.
+            frame.add(this.gui);
+            // Display the window.
+            frame.pack();
+            frame.setVisible(true);
+        } else if (o instanceof ClientState) {
+            // Updates the ContextVector of the GUI with the one sent by the server
+            CollabModel collab = this.gui.getCollabModel();
+            collab.setCV((ClientState) o);
+        } else if (o instanceof Pair<?, ?>) {
+            // Updates list of current users and documetns
+            ArrayList<String> users = ((Pair<ArrayList<String>, ArrayList<String>>) o).first;
+            ArrayList<String> documents = ((Pair<ArrayList<String>, ArrayList<String>>) o).second;
+            this.gui.updateUsers(users.toArray());
+            this.gui.updateDocumentsList(documents.toArray());
+        } else {
+            throw new RuntimeException("Unrecognized object type received by client");
+        }
+	}
+
+	/**
+	 * Updates the client's copy of the document using operational transform
+	 * through a call to the CollabModel's remoteInsert/remoteDelete
+	 * @param o - the operation that was received from the server to
+	 * apply to the model
+	 */
+	@Override
+	public void updateDoc(Operation o) {
+		try {
+			if (o instanceof InsertOperation) {
+				this.gui.getCollabModel().remoteInsert((Operation) o);
+			} else if (o instanceof DeleteOperation) {
+				this.gui.getCollabModel().remoteDelete((Operation) o);
+			} else if (o instanceof UpdateOperation) {
+				throw new UnsupportedOperationException();
+			} else {
+				throw new RuntimeException("Shouldn't reach here");
+			}
+		} catch (OperationEngineException e) {
+			new ErrorDialog(e.toString());
+		} catch (BadLocationException e) {
+			new ErrorDialog(e.toString());
+		}
+	}
+
+	/**
+	 * @return the siteID of the document
+	 */
+	@Override
+	public int getID() {
+		return siteID;
+	}
+
+	/**
+	 * Transmits local changes to the server via an operatoin
+	 * @param the operation to transmit to server
+	 * @throws IOException if the OutputStream is corrupted or broken
+	 */
+	@Override
+	public void transmit(Operation o) throws IOException {
+		if (out == null)
+			throw new RuntimeException("Socket not initialized.");
+		out.writeObject(o);
+		out.flush();
+	}
+
+	/**
+	 * Used by the document selector popup to set the
+	 * @param new name of the document
+	 */
+	public void setDocument(String text) {
+		document = text;
+	}
+
+	/** @return ip address of client */
+	public String getIP() {
+	    return this.ip;
+	}
+
+	/** @return port number of client */
+	public int getPort() {
+	    return this.port;
+	}
+
+	/** @return username of client */
+	public String getUsername() {
+	    return this.name;
+	}
+
+}
